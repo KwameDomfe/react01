b84f106d11843dda901d48ec327fdc0f5d3d600a
KwameDomfe <nkadbasics@gmail.com>
2025-04-25 04:37:23 +0000

    Updated App.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import HeroSection from '../components/organisms/HeroSection'
-import CorePurposeSection from '../components/organisms/CorePurposeSection'
-import CoreServicesSection from '../components/organisms/CoreServicesSection'
-import ProjectsSection from '../components/organisms/ProjectsSection'
-import GlobabuildSection from '../components/organisms/GlobabuildSection'
-import AddressSection from '../components/organisms/AddressSection'
+// import HeroSection from '../components/organisms/HeroSection'
+// import CorePurposeSection from '../components/organisms/CorePurposeSection'
+// import CoreServicesSection from '../components/organisms/CoreServicesSection'
+// import ProjectsSection from '../components/organisms/ProjectsSection'
+// import GlobabuildSection from '../components/organisms/GlobabuildSection'
+// import AddressSection from '../components/organisms/AddressSection'
 import FAQsSection from '../components/organisms/FAQsSection'
 
 export default function Home(){
